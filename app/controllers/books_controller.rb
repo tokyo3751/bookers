1b2a204c42da0d06dbfae70fe682c7b9f23529e7
EC2 Default User <ec2-user@ip-172-31-23-107.ec2.internal>
2022-03-15 08:26:38 +0000

    [Finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,54 @@
 class BooksController < ApplicationController
   def new
+    @books = Books.new
   end
-
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end  
+     
+  end
+  
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      @books = Book.all
+      render :edit
+    end  
+  end
+  
+  def destroy
+    book = Book.find(params[:id]) 
+    book.destroy  
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'  
+  end
+  
+ private
+ 
+ def book_params
+   params.require(:book).permit(:title,:body)
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get '/top' => "homes#top"
+  root :to  => "homes#top"
   resources :books
+  post 'books' => 'books#create'
+  get 'books/:id' =>'books#show'
+  get 'books/:id/edit' => 'books#edit'
+  patch 'books/:id' => 'books#update'
 end
